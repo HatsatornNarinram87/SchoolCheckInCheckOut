40141086567ec68dadfb4a5035b9798e58ccda0e
Tawan <hatsatorn.narinram@gmail.com>
2026-05-17 20:30:05 +0700

    prod

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
   SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxaE4bDRRdOEvbQM-W_UDHM4EStBLE_C83VEGKHNkpIIGsGVzZYlRXOdOhyzQLNIB0P/exec',
 
   // Google OAuth Client ID (จาก Google Cloud Console)
-  GOOGLE_CLIENT_ID: '921804252606-5tepbu13r8q4e61d9ktcbs7tfmkpaebt.apps.googleusercontent.com',
-  // GOOGLE_CLIENT_ID: '921804252606-oebjar2gqt4b7kp8j99ur3nnm5h5sc8t.apps.googleusercontent.com', //prod
+  // GOOGLE_CLIENT_ID: '921804252606-5tepbu13r8q4e61d9ktcbs7tfmkpaebt.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '921804252606-oebjar2gqt4b7kp8j99ur3nnm5h5sc8t.apps.googleusercontent.com', //prod
 
   // Domain อีเมลโรงเรียน (ถ้าไม่จำกัด domain ให้ใส่ '')
   SCHOOL_EMAIL_DOMAIN: '',
